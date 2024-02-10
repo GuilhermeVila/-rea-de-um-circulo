2b0b3bcbc196eae7837bf551b91c805da4c28cca
GuilhermeVila <guilherme.vilan@gmail.com>
2024-02-10 17:31:49 -0300

    adicionar volume da Esfera

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function NomeInicial(tag, texto){
     let camp = document.querySelector(tag);
     camp.innerHTML = texto}
-function areaCirculo(r){
+function volumeEsfera(r){
+    let result = ((r ** 3)*(4*Math.PI)/3);
+    return result;
+}
+    function areaCirculo(r){
     let resultado = ((r*r)*Math.PI);
     return resultado;
 }
-console.log(areaCirculo(4));
-NomeInicial("h1", "calculadora de area");
+NomeInicial("h1", "calculadora de area e volume");
 NomeInicial("p", "determine o raio");
+console.log(areaCirculo(4));
+console.log(volumeEsfera(2))
