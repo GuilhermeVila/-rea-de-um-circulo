5ab346b2ed0c6f8faef6bef2507d847eb4acaca6
GuilhermeVila <guilherme.vilan@gmail.com>
2024-02-10 02:05:40 -0300

    texto simples

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+function NomeInicial(tag, texto){
+    let camp = document.querySelector(tag);
+    camp.innerHTML = texto}
 function areaCirculo(r){
     let resultado = ((r*r)*Math.PI);
     return resultado;
 }
 console.log(areaCirculo(4));
+NomeInicial("h1", "calculadora de area");
+NomeInicial("p", "determine o raio")
