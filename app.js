ffb38666feae4a7488bbd5e4c3dea57c7de85d82
GuilhermeVila <guilherme.vilan@gmail.com>
2024-02-10 17:42:22 -0300

    Revert "adicionar volume da Esfera"
    This reverts commit 2b0b3bcbc196eae7837bf551b91c805da4c28cca.

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 function NomeInicial(tag, texto){
     let camp = document.querySelector(tag);
     camp.innerHTML = texto}
-function volumeEsfera(r){
-    let result = ((r ** 3)*(4*Math.PI)/3);
-    return result;
-}
-    function areaCirculo(r){
+function areaCirculo(r){
     let resultado = ((r*r)*Math.PI);
     return resultado;
 }
-NomeInicial("h1", "calculadora de area e volume");
-NomeInicial("p", "determine o raio");
 console.log(areaCirculo(4));
-console.log(volumeEsfera(2))
+NomeInicial("h1", "calculadora de area");
+NomeInicial("p", "determine o raio");
